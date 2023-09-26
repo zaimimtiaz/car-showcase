2a0776e4cb5ae6ed62659c3909dd55e3d2384bf6
zaim imtiaz <117397927+zaimimtiaz@users.noreply.github.com>
2023-09-26 20:28:57 +0000

    created button component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
 import image from 'next/image';
+import CustomButton from './CustomButton';
+
 const Hero = () => {
+  const handleScroll = () = {
+
+  }
   return (
-    <div>
-      Hero
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>
+          Find, book or rent a car - quickly and easily!
+        </h1>
+        <p className='hero__subtitle'>
+          Streamline your car rental exoerience with our effortless booking process.
+        </p>
+
+        <CustomButton 
+        title="Explore Cars"
+        containerstyles="bg-primary-blue text-white rounded-full mt-10"
+        handleClick={handleScroll}
+        />
+      </div>
     </div>
   )
 }
